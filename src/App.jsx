a1be91cfa2aeef5f8c6b589da2ce79e3ca2bc907
Cynthia Costa <cyxcosta@gmail.com>
2020-08-22 13:29:26 -0300

    Configure folders structure, remove unused files and implemente coding rules

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// Main style
+import './assets/stylesheets/app.scss'
+
+function App() {
+  return (
+    <div>
+      Hello, world!
+    </div>
+  );
+}
+
+export default App;
