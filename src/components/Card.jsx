8ac8ec837fffe72332e86aa3a8d21cec1339f630
Cynthia Costa <cyxcosta@gmail.com>
2020-08-23 19:03:33 -0300

    Implement API request and render cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
+import placeholder from '../assets/images/placeholder.jpg'
 
 // Components
 import Heart from './icons/Heart'
 import Eye from './icons/Eye'
 
-function Card() {
+function Card({ poster, title, year }) {
+  const image = poster === 'N/A' ? placeholder : poster
+
   return (
     <li className="card">
       <figure className="card__figure">
-        <img className="card__image" src="https://via.placeholder.com/310x460" alt="" />
+        <img className="card__image" src={image} alt={title} />
         <figcaption className="card__caption">
-          <h2 className="card__title">Movie Title</h2>
-          <span className="card__year">2020</span>
+          <h2 className="card__title">{title}</h2>
+          <span className="card__year">{year}</span>
         </figcaption>
       </figure>
       <button className="card__bookmark">
@@ -22,6 +27,12 @@ function Card() {
       </button>
     </li>
   )
+}
+
+Card.propTypes = {
+  poster: PropTypes.string,
+  title: PropTypes.string,
+  year: PropTypes.string
 }
 
 export default Card
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 // Components
 import Card from './Card'
 
-function List() {
+function List({ movies, errorMessage }) {
+  console.log(movies)
   return (
     <ul className="list">
-      <Card />
+      {movies.length > 0 ? movies.map(({ imdbID, Poster, Title, Year }) => (
+        <Card key={imdbID} poster={Poster} title={Title} year={Year} />
+      )) : (
+        <li className="list__noresult">{errorMessage}</li>
+      )}
     </ul>
   )
+}
+
+List.propTypes = {
+  movies: PropTypes.array,
+  errorMessage: PropTypes.string
 }
 
 export default List
