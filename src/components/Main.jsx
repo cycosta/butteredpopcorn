c28f8f7bfc4080eda56515e439144a6f20d552d3
Cynthia Costa <cyxcosta@gmail.com>
2020-08-23 14:12:11 -0300

    Include illustration for default/blank screen and other smaller adjustments

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 // Components
 import Search from './Search'
 import List from './List'
+import HomeCinema from './icons/HomeCinema'
 
 function Main() {
   return (
     <main className="main">
       <Search />
+      <HomeCinema />
       <List />
     </main>
   )
