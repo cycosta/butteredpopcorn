f5313f42726db58ee0061ed7cbc8ef37316409c4
Cynthia Costa <cyxcosta@gmail.com>
2020-08-23 20:21:40 -0300

    Movie page initial structure and style

--- src/Movie.jsx
+++ src/Movie.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 
+import { getMovie } from './services/api'
+import placeholder from './assets/images/placeholder.jpg'
+
 function Movie({ movieId }) {
+  const [movie, setMovie] = useState({})
+
+  useEffect(() => {
+    getMovie(movieId).then((data) => setMovie(data))
+  }, [movieId])
+  console.log(movie)
+
+  const image = movie.Poster === 'N/A' ? placeholder : movie.Poster
+
   return (
-    <div>{movieId}</div>
+    <main className="main">
+      <div className="movie">
+        <div className="movie__info">
+          <h1 className="movie__title">{movie.Title}</h1>
+          <h2 className="movie__label">Plot</h2>
+          <p className="movie__plot">{movie.Plot}</p>
+        </div>
+        <div className="movie__poster">
+          <img className="movie__image" src={image} alt={movie.Title} />
+        </div>
+      </div>
+    </main>
   )
 }
 
