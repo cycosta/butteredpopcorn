ce38e10403e527bf056c18b3635458677c2461cd
Cynthia Costa <cyxcosta@gmail.com>
2020-08-27 11:56:30 -0300

    Adjust movie page container

--- src/Movie.jsx
+++ src/Movie.jsx
@@ -37,59 +37,57 @@ function Movie({ movieId }) {
   const image = Poster === 'N/A' ? placeholder : Poster
 
   return (
-    <main className="main">
-      <div className="movie">
-        <Link to="/" className="movie__return">
-          <Arrow />
-        </Link>
-        <div className="movie__content">
-          <div className="movie__info">
-            <div className="movie__header-info">
-              <p className="movie__header-item">{Runtime}</p>
-              <p className="movie__header-item">{Year}</p>
-              <p className="movie__header-item">{Rated}</p>
-            </div>
-            <div className="movie__main-info">
-              <h1 className="movie__title">{Title}</h1>
-              <h2 className="movie__label">Plot</h2>
-              <p className="movie__text movie__text--plot">{Plot}</p>
-              <h2 className="movie__label">Cast</h2>
-              <p className="movie__text">{Actors}</p>
-              <h2 className="movie__label">Genre</h2>
-              <p className="movie__text">{Genre}</p>
-              <h2 className="movie__label">Director</h2>
-              <p className="movie__text">{Director}</p>
-            </div>
-            <ul className="movie__ratings">
-              <li className="movie__rating">
-                <div className="movie__rating-icon">
-                  <img className="movie__rating-img" src={imdbIcon} alt="IMDB Icon" />
-                </div>
-                <div className="movie__rating-info">
-                  <p className="movie__rating-value">{imdbRating?.Value}</p>
-                  <span className="movie__rating-label">IMDB</span>
-                </div>
-              </li>
-              <li className="movie__rating">
-                <div className="movie__rating-icon">
-                  <img className="movie__rating-img" src={rottenIcon} alt="Rotten Tomatoes Icon" />
-                </div>
-                <div className="movie__rating-info">
-                  <p className="movie__rating-value">{rottenRating?.Value}</p>
-                  <span className="movie__rating-label">Rotten Tomatoes</span>
-                </div>
-              </li>
-            </ul>
+    <div className="movie">
+      <Link to="/" className="movie__return">
+        <Arrow />
+      </Link>
+      <div className="movie__content">
+        <div className="movie__info">
+          <div className="movie__header-info">
+            <p className="movie__header-item">{Runtime}</p>
+            <p className="movie__header-item">{Year}</p>
+            <p className="movie__header-item">{Rated}</p>
           </div>
-          <div className="movie__poster">
-            <img className="movie__image" src={image} alt={Title} />
-            <button className="movie__bookmark">
-              <Heart movieId={movieId} />
-            </button>
+          <div className="movie__main-info">
+            <h1 className="movie__title">{Title}</h1>
+            <h2 className="movie__label">Plot</h2>
+            <p className="movie__text movie__text--plot">{Plot}</p>
+            <h2 className="movie__label">Cast</h2>
+            <p className="movie__text">{Actors}</p>
+            <h2 className="movie__label">Genre</h2>
+            <p className="movie__text">{Genre}</p>
+            <h2 className="movie__label">Director</h2>
+            <p className="movie__text">{Director}</p>
           </div>
+          <ul className="movie__ratings">
+            <li className="movie__rating">
+              <div className="movie__rating-icon">
+                <img className="movie__rating-img" src={imdbIcon} alt="IMDB Icon" />
+              </div>
+              <div className="movie__rating-info">
+                <p className="movie__rating-value">{imdbRating?.Value}</p>
+                <span className="movie__rating-label">IMDB</span>
+              </div>
+            </li>
+            <li className="movie__rating">
+              <div className="movie__rating-icon">
+                <img className="movie__rating-img" src={rottenIcon} alt="Rotten Tomatoes Icon" />
+              </div>
+              <div className="movie__rating-info">
+                <p className="movie__rating-value">{rottenRating?.Value}</p>
+                <span className="movie__rating-label">Rotten Tomatoes</span>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div className="movie__poster">
+          <img className="movie__image" src={image} alt={Title} />
+          <button className="movie__bookmark">
+            <Heart movieId={movieId} />
+          </button>
         </div>
       </div>
-    </main>
+    </div>
   )
 }
 
