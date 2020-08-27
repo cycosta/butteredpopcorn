33d249273fb9c6e37ee4ca1d39d44bd2acdb78eb
Cynthia Costa <cyxcosta@gmail.com>
2020-08-27 16:33:09 -0300

    Remove comment

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -5,33 +5,22 @@ import PropTypes from 'prop-types'
 import ReactPagination from "react-js-pagination";
 
 function Pagination({ totalResults, setPage, page }) {
-  // const [pages, setPages] = useState(0)
-
-  // useEffect(() => {
-  //   if (!totalResults) return
-  //   setPages(getPages(totalResults))
-  // }, [totalResults])
-
-  // function getPages(n) {
-  //   return Math.ceil(parseInt(n) / 10)
-  // }
-
   return (
     <div>
-        <ReactPagination
-          activePage={page}
-          itemsCountPerPage={10}
-          totalItemsCount={parseInt(totalResults)}
-          pageRangeDisplayed={10}
-          innerClass={'pagination'}
-          itemClass={'pagination__page'}
-          linkClass={'pagination__link'}
-          activeClass={'pagination__page--active'}
-          onChange={(clickedPage) => setPage(clickedPage)}
-          prevPageText={'‹'}
-          nextPageText={'›'}
-        />
-      </div>
+      <ReactPagination
+        activePage={page}
+        itemsCountPerPage={10}
+        totalItemsCount={parseInt(totalResults)}
+        pageRangeDisplayed={10}
+        innerClass={'pagination'}
+        itemClass={'pagination__page'}
+        linkClass={'pagination__link'}
+        activeClass={'pagination__page--active'}
+        onChange={(clickedPage) => setPage(clickedPage)}
+        prevPageText={'‹'}
+        nextPageText={'›'}
+      />
+    </div>
   )
 }
 
